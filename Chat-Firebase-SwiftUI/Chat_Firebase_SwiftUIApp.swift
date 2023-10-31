2d93a02593ed28b4b8076b31c05ce83dec291b9a
Cengizhan Tomak <cengizhan.tomak@lvs.com.tr>
2023-10-31 12:00:37 +0300

    Initial Commit

---
+++ Chat-Firebase-SwiftUI/Chat_Firebase_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_Firebase_SwiftUIApp.swift
+//  Chat-Firebase-SwiftUI
+//
+//  Created by Cengizhan Tomak on 31.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Chat_Firebase_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
