defa5ef30056f8a16e1d35912cee140db6f83fc3
Cengizhan Tomak <cengizhantomak@gmail.com>
2023-11-02 11:50:18 +0300

    Creating Template Messages View

---
+++ Chat-Firebase-SwiftUI/MainMessages/MainMessagesView.swift
@@ -0,0 +1,124 @@
+//
+//  MainMessagesView.swift
+//  Chat-Firebase-SwiftUI
+//
+//  Created by Cengizhan Tomak on 31.10.2023.
+//
+
+import SwiftUI
+
+struct MainMessagesView: View {
+    @State var shouldShowLogOutOptions = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                customNavBar
+                messagesView
+            }
+            .overlay(newMessageButton, alignment: .bottom)
+            .navigationBarHidden(true)
+        }
+    }
+    
+    private var customNavBar: some View {
+        HStack(spacing: 16) {
+            Image(systemName: "person.fill")
+                .font(.system(size: 34, weight: .heavy))
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text("USERNAME")
+                    .font(.system(size: 24, weight: .bold))
+                
+                HStack {
+                    Circle()
+                        .foregroundStyle(.green)
+                        .frame(width: 14, height: 14)
+                    Text("online")
+                        .font(.system(size: 12))
+                        .foregroundStyle(.secondary)
+                }
+            }
+            
+            Spacer()
+            
+            Button {
+                shouldShowLogOutOptions.toggle()
+            } label: {
+                Image(systemName: "gear")
+                    .font(.system(size: 24, weight: .bold))
+                    .foregroundStyle(.primary)
+            }
+        }
+        .padding()
+        .actionSheet(isPresented: $shouldShowLogOutOptions) {
+            .init(title: Text("Settings"), message: Text("What do you want to do?"), buttons: [
+                .destructive(Text("Sign Out"), action: {
+                    print("handle sign out")
+                }),
+                .cancel()
+            ])
+        }
+    }
+    
+    private var messagesView: some View {
+        ScrollView {
+            ForEach(0..<10, id: \.self) { num in
+                VStack {
+                    HStack(spacing: 16) {
+                        Image(systemName: "person.fill")
+                            .font(.system(size: 32))
+                            .padding(8)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 44)
+                                    .stroke(.primary, lineWidth: 1)
+                            )
+                        
+                        VStack(alignment: .leading) {
+                            Text("Username")
+                                .font(.system(size: 16, weight: .bold))
+                            Text("Message sent to user")
+                                .font(.system(size: 14))
+                                .foregroundStyle(.secondary)
+                        }
+                        
+                        Spacer()
+                        
+                        Text("22d")
+                            .font(.system(size: 14, weight: .semibold))
+                    }
+                    Divider()
+                        .padding(.vertical, 8)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom, 50)
+        }
+    }
+    
+    private var newMessageButton: some View {
+        Button {
+            
+        } label: {
+            HStack {
+                Spacer()
+                Text("+ New Message")
+                    .font(.system(size: 16, weight: .bold))
+                Spacer()
+            }
+            .foregroundStyle(.white)
+            .padding(.vertical)
+            .background(.blue)
+            .cornerRadius(32)
+            .padding(.horizontal)
+            .shadow(radius: 15)
+        }
+    }
+}
+
+#Preview {
+    MainMessagesView()
+        .preferredColorScheme(.dark)
+    
+//    MainMessagesView()
+}
